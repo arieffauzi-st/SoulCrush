03d81d8e3e048f38bc1ba855ef1ce1ca1e149015
arieffauzi-st <arieffauzi_st@gmail.com>
2024-09-17 01:51:43 +0700

    feat(routing): Add bootstrap theme and set up Angular routing

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,12 +4,15 @@ import { provideAnimations }from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { ɵAnimationGroupPlayer } from '@angular/animations';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
-    provideHttpClient(),
-    provideAnimations()
+    provideHttpClient(), 
+    provideAnimations(),
+    provideToastr({
+      positionClass: 'toast-bottom-right'
+    })
   ]
 };
